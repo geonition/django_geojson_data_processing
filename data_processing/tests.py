ba60af2938394f3df752d78813f2374ba08381d0
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-09-27 14:32:05 +0300

    the format change urls from geojson or from json to csv works now,, more testing is needed but at least the initial functionallity is there. Please report any found bugs

--- data_processing/tests.py
+++ data_processing/tests.py
@@ -1,16 +1,104 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import Client
+from django.core.urlresolvers import reverse
+from django.utils import simplejson as json
 
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class FormatTest(TestCase):
+    
+    def setUp(self):
+        
+        self.client = Client()
+        
+        self.json_dict = {
+            'array': [1,2,3],
+            'boolean': True,
+            'key': 'value',
+            'null_value': None,
+            'number': 1,
+            'object': {
+                'nested': True
+            },
+            'string': 'some; :tricky. ,string;'
+        }
+        
+        self.correct_json_to_csv = u"%s%s%s%s%s%s%s%s" % ( 
+        "array;boolean;key;null_value;number;object.nested;string;\n",
+        "[1, 2, 3];",
+        "true;",
+        "\"value\";",
+        "null;",
+        "1;",
+        "true;",
+        "\"some :tricky. ,string\";\n") #remove ; characters
+            
+        self.geojson_dict = {
+            'type': 'FeatureCollection',
+            'features': [
+                {'type': 'Feature',
+                 'id': 1,
+                 'geometry': {
+                    'type': 'Point',
+                    'coordinates': [1,2],
+                    },
+                'properties': self.json_dict
+                },
+                {'type': 'Feature',
+                 'id': 2,
+                 'geometry': {
+                    'type': 'Polygon',
+                    'coordinates': [[
+                        [100.0, 0.0],
+                        [101.0, 0.0],
+                        [101.0, 1.0],
+                        [100.0, 1.0],
+                        [100.0, 0.0]]]
+                    },
+                'properties': {}
+                },
+                {'type': 'Feature',
+                 'geometry': {
+                    'type': 'LineString',
+                    'coordinates': [
+                        [100.0, 0.0],
+                        [101.0, 1.0]]
+                        },
+                'properties': {
+                    'some_unique_val': 'yes; this is it'
+                    }
+                }
+            ]
+        }
+        
+        self.correct_geojson_to_csv = u"%s%s%s%s%s%s%s" % (
+        "id;geometry_wkt;array;boolean;key;null_value;number;",
+        "object.nested;some_unique_val;string;\n",
+        "1;\"POINT (1.0000000000000000 2.0000000000000000)\";[1, 2, 3];true;\"value\";null;1;true;\"\";",
+        "\"some :tricky. ,string\";\n", #remove ; characters
+        "2;\"POLYGON ((100.0000000000000000 0.0000000000000000, 101.0000000000000000 0.0000000000000000, 101.0000000000000000 1.0000000000000000, 100.0000000000000000 1.0000000000000000, 100.0000000000000000 0.0000000000000000))\";",
+        "\"\";\"\";\"\";\"\";\"\";\"\";\"\";\"\";\n", 
+        "null;\"LINESTRING (100.0000000000000000 0.0000000000000000, 101.0000000000000000 1.0000000000000000)\";\"\";\"\";\"\";\"\";\"\";\"\";\"yes this is it\";\"\";\n") 
+        
+    def test_json_to_csv(self):
+        #send json to url
+        response = self.client.post(reverse('json_to_csv'),
+                                    data=json.dumps([self.json_dict]),
+                                    content_type='application/json')
+        
+        #check that the same info can be found from the
+        #returned csv
+        self.assertEquals(response.content,
+                          self.correct_json_to_csv,
+                          "The csv response was not correct")
+        
+    def test_geojson_to_csv(self):
+        #send json to url
+        response = self.client.post(reverse('geojson_to_csv'),
+                                    data=json.dumps(self.geojson_dict),
+                                    content_type='application/json')
+        
+        #check that the same info can be found from the
+        #returned csv
+        self.assertEquals(response.content,
+                          self.correct_geojson_to_csv,
+                          "The csv response was not correct")
+        
--- data_processing/views.py
+++ data_processing/views.py
@@ -1,1 +1,165 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.gis.geos import WKTWriter
+from django.utils import simplejson as json
+import types
+
+def geojson_to_csv(request):
+    """
+    This function should recieve a post request with
+    a featurecollection.
+    
+    From this featurecollection this function will make a csv
+    string and return it as a csv file for download.
+    """
+    json_string = request.raw_post_data
+    
+    feature_collection = json.loads(json_string)
+    
+    features = feature_collection['features']
+    
+    csv_header_set = set()
+    for feature in features:
+        csv_header_set.update(create_csv_header_set(feature['properties']))
+    
+    csv_header_list = list(csv_header_set)
+    csv_header_list.sort()
+    csv_header_list.insert(0, 'geometry_wkt')
+    csv_header_list.insert(0, 'id')
+    
+    #create the csv
+    csv_string = u""
+    
+    #write the header
+    for header in csv_header_list:
+        csv_string = u"%s%s;" % (csv_string, header)
+    
+    csv_string = u"%s\n" % (csv_string,)
+    
+    for feature in features:
+        
+        geometry = feature['geometry']
+        properties = feature['properties']
+        
+        id = None
+        if feature.has_key('id'):
+            id = feature['id']
+        
+        properties['id'] = id
+        
+        #make the geometry
+        ggeom = GEOSGeometry(json.dumps(feature['geometry']))
+        wkt_w = WKTWriter()
+        properties['geometry_wkt'] = wkt_w.write(ggeom)
+        
+        value_list = get_value_list(properties, csv_header_list)
+        
+        for value in value_list:
+            
+            #remove harmful characterers
+            if type(value) == types.UnicodeType or type(value) == types.StringType:
+                value = value.replace(";", "")
+            
+            #modify value to json string
+            value = json.dumps(value)
+            
+            csv_string = "%s%s;" % (csv_string, value)
+            
+        csv_string = u"%s\n" % (csv_string,)
+    
+    return HttpResponse(csv_string,
+                        content_type='text/csv')
+    
+
+def json_to_csv(request):
+    """
+    This function takes a array of json objects
+    and returns a csv file for download.
+    """
+    json_string = request.raw_post_data
+    
+    json_array = json.loads(json_string)
+    
+    #this part has to check for nested objects
+    csv_header_set = set()
+    for json_dict in json_array:
+        csv_header_set.update(create_csv_header_set(json_dict))
+
+    csv_header_list = list(csv_header_set)
+    csv_header_list.sort()
+
+    #create the csv
+    csv_string = u""
+    
+    #write the header
+    for header in csv_header_list:
+        csv_string = u"%s%s;" % (csv_string, header)
+    
+    csv_string = u"%s\n" % (csv_string,)
+    
+    for json_dict in json_array:
+        value_list = get_value_list(json_dict, csv_header_list)
+        
+        for value in value_list:
+            
+            #remove harmful characterers
+            if type(value) == types.UnicodeType:
+                value = value.replace(";", "")
+            
+            #modify value to json string
+            value = json.dumps(value)
+            
+            csv_string = u"%s%s;" % (csv_string, value)
+            
+        csv_string = u"%s\n" % (csv_string,)
+        
+    return HttpResponse(csv_string,
+                        content_type="text/csv")
+   
+def create_csv_header_set(json_dict):
+    
+    csv_header_set = set()
+    
+    for key, item in json_dict.items():
+        if type(item) == types.DictType:
+            temp_set = create_csv_header_set(item)
+            for header in temp_set:
+                add_header = "%s.%s" % (key, header)
+                csv_header_set.add(add_header)
+        else:
+            csv_header_set.add(key)
+            
+    return csv_header_set
+
+def get_value_list(json_dict, key_list):
+    
+    value_list = []
+    
+    for key in key_list:
+        
+        keys = key.split(".")
+        
+        if len(keys) == 1:
+            if json_dict.has_key(key):
+                value_list.append(json_dict[key])
+            else:
+                value_list.append("")
+        else:
+            temp_key = ""
+            
+            for k in keys[1:]:
+                if temp_key != "":
+                    temp_key = temp_key + "." + k
+                else:
+                    temp_key = k
+            
+            if json_dict.has_key(keys[0]):
+                temp_value_list = get_value_list(json_dict[keys[0]],
+                                                 [temp_key])
+            else:
+                temp_value_list = get_value_list({},
+                                                 [temp_key])
+                
+            value_list.extend(temp_value_list)
+    
+    return value_list
