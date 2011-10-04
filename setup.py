b64352143930e942f094db0fb3ff7cc02bc77e6e
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-10-04 21:21:01 +0300

    setup file version changed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='data_processing',
-    version='1.0.0',
+    version='1.0.1',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/django_geojson_data_processing',
     packages=find_packages(),
