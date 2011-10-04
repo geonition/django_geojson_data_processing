9241ebccd66f29706d05de6fe7a619b309715772
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-10-04 20:38:19 +0300

    javascript function for formating geojson featurecollection added

--- data_processing/templates/data_processing.jquery.js
+++ data_processing/templates/data_processing.jquery.js
@@ -7,7 +7,7 @@ gnt['data_processing'] = {}
  to the server and recieved a csv file
  back with the information in the json
 */
-gnt.data_processing['csv_from_json'] =
+gnt.data_processing['json_to_csv'] =
     function(json_object, callback_function) {
         console.log("send post");
         $.ajax({
@@ -33,4 +33,33 @@ gnt.data_processing['csv_from_json'] =
         });
     }
 
-
+/*
+ This function sends a geojson featurecollection
+ to the server and recieved a csv file
+ back with the information in the geojson featurecollection
+*/
+gnt.data_processing['geojson_to_csv'] =
+    function(json_object, callback_function) {
+        console.log("send post");
+        $.ajax({
+            url: "{% url geojson_to_csv %}",
+            type: "POST",
+            data: JSON.stringify(json_object),
+            contentType: "application/json",
+            success: function(data){
+                if(callback_function !== undefined) {
+                    callback_function(data);
+                    }
+                },
+            error: function(e) {
+                if(callback_function !== undefined) {
+                    callback_function(e);
+                }
+            },
+            dataType: "json",
+            beforeSend: function(xhr){
+                //for cross site authentication using CORS
+                xhr.withCredentials = true;
+                }
+        });
+    }
