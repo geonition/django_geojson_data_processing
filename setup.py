fa07dc56a3c366d70f23f3071dd907448feeda9f
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-10-04 21:15:20 +0300

    setup file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='data_processing',
+    version='1.0.0',
+    author='Kristoffer Snabb',
+    url='https://github.com/geonition/django_geojson_data_processing',
+    packages=['data_processing'],
+)
