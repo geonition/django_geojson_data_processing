d69637bfb741a92c358df65f9d2ca6c0d588f4fd
Kristoffer Snabb <ksnabb@ubuntu-virtual.(none)>
2012-02-20 14:52:53 +0200

    fixed another bug with string added if value empty

--- data_processing/templates/data_processing.jquery.js
+++ data_processing/templates/data_processing.jquery.js
@@ -8,11 +8,17 @@ gnt['data_processing'] = {}
  back with the information in the json
 */
 gnt.data_processing['json_to_csv'] =
-    function(json_object, callback_function) {
+    function(json_string_or_object, callback_function) {
+
+        var json_string = json_string_or_object;
+        if( typeof(" ") !== typeof(json_string_or_object)) {
+            json_string = JSON.stringify(json_string_or_object);
+        }
+
         $.ajax({
             url: "{% url json_to_csv %}",
             type: "POST",
-            data: JSON.stringify(json_object),
+            data: json_string,
             contentType: "application/json",
             success: function(data){
                 if(callback_function !== undefined) {
@@ -38,11 +44,17 @@ gnt.data_processing['json_to_csv'] =
  back with the information in the geojson featurecollection
 */
 gnt.data_processing['geojson_to_csv'] =
-    function(json_object, callback_function) {
+    function(feature_collection, callback_function) {
+
+        var json_string = feature_collection;
+        if( typeof(" ") !== typeof(feature_collection)) {
+            json_string = JSON.stringify(feature_collection);
+        }
+
         $.ajax({
             url: "{% url geojson_to_csv %}",
             type: "POST",
-            data: JSON.stringify(json_object),
+            data: json_string,
             contentType: "application/json",
             success: function(data){
                 if(callback_function !== undefined) {
